a02c8a568a98c38e2f700f8536664d4770b04d80
unknown <PT817@CSLAB126.CSSERVER.local>
2017-08-05 18:54:32 +0800

    fist

---
+++ MvcApplication3/Models/Information.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication3.Models
+{
+    public class Information
+    {
+        public List<Account> Accounts;
+
+        public Information()
+        { 
+            Accounts = new List<Account>();
+            Accounts.Add(new Account { Id = 1, Name = "aaron neri", Age = 15 });
+            Accounts.Add(new Account { Id = 2, Name = "john wall", Age = 16 });
+            Accounts.Add(new Account { Id = 3, Name = "george smith", Age = 17 });
+        }
+    }
+}
